b6fffcc2c06fb085a95bc155509cbfc8168b8118
akifkerimgunsal <akifkerimgunsal@gmail.com>
2024-03-19 17:20:41 +0300

    Guncelleme

---
+++ 6_Nesneye Dayalı Programlama_2. Alıştırmalar/EncapsulationOrnek/EncapsulationOrnek/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationOrnek
+{
+    internal class Person
+    {
+        private string name;
+                
+        public string Name
+        {            
+            get
+            {
+                return name;
+            }            
+            set
+            {
+                if (value != null && value != "")
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine("Geçersiz isim.");
+                }
+            }
+        }
+    }
+}
---
+++ 6_Nesneye Dayalı Programlama_2. Alıştırmalar/EncapsulationOrnek/EncapsulationOrnek/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EncapsulationOrnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();            
+            person.Name = "John";
+            
+            Console.WriteLine("Kişinin adı: " + person.Name);           
+            person.Name = ""; 
+        }
+    }
+}
